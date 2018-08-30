934bcc16e9f5b963edb3c1faa1d25841dc5d663c
cb <cb@cbcorp.com>
2018-08-30 19:03:45 +0200

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -1,2 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+
+const user = {
+  name: 'Francis',
+  lastname: 'Jones',
+  age: 16
+}
+
+const Header = () => {
+  return (
+    <div>
+      <div>{user.name}</div>
+      <div>{user.lastname}</div>
+      <div>{user.age}</div>
+    </div>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// Components
+import Header from './components/header'
+
 const App = () => {
-  return <div>Header</div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 ReactDom.render(<App />, document.querySelector('#root'))
