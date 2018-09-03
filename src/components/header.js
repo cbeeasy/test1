5068ae8a27738c9df507288f11b4084655e8aabd
cb <cb@cbcorp.com>
2018-09-03 17:43:23 +0200

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import "../css/styles.css";
+import React, { Component } from 'react'
+import '../css/styles.css'
 
 class Header extends Component {
   state = {
     active: false
-  };
+  }
 
-  inputChangeHandler = event => {
-    const value = event.target.value !== "";
+  inputChangeHandler = (event) => {
+    const value = event.target.value !== ''
 
     this.setState({
       active: value
-    });
-  };
+    })
+  }
 
-  render() {
+  render () {
     return (
-      <header>
-        <div className="logo">Logo</div>
-        <input type="text" onChange={this.inputChangeHandler} />
+      <header style={{ background: `${this.state.active ? 'red' : 'blue'}` }}>
+        <div className='logo'>Logo</div>
+        <input type='text' onChange={this.inputChangeHandler} />
       </header>
-    );
+    )
   }
 }
 
-export default Header;
+export default Header
