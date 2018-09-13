51c3e4fa1a0679c040ab0e0134abe66a7541c5b9
cb <cb@cbcorp.com>
2018-09-13 21:46:02 +0200

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import '../css/styles.css'
+import classes from '../css/styles.css'
 
 class Header extends Component {
   state = {}
@@ -9,7 +9,7 @@ class Header extends Component {
   render () {
     return (
       <header>
-        <div className='logo'>Logo</div>
+        <div className={classes.logo}>Logo</div>
         <input type='text' onChange={this.inputChangeHandler} />
       </header>
     )
--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from '../css/styles.css'
 
 const NewsItem = ({ item }) => (
-  <div className={classes.new_item}>
+  <div className={classes.news_item}>
     <h3>{item.title}</h3>
     <div>{item.feed}</div>
   </div>
